d3c40275fab2f469f16a4677c4ce50e7eeecb976
Malal <controlmalal@gmail.com>
2017-08-18 23:24:56 +0000

    first part of function generate

--- jquery.gf.js
+++ jquery.gf.js
@@ -1,8 +1,26 @@
-$(function(){
+$(function () {
     var json = [{
-        "username":"text"
+        "text": "username",
+        "placeholder": "Votre nom utilisateur"
     },
-       {
-          "password":"password" 
-       }]
+    {
+        "password": "password"
+    },
+    {
+        "email": "email"
+    },
+    {
+        "url": "Votre URL"
+    }]
+    
+    $.each(json, function(index, data){
+        if(data.text) {
+            $(".gf").append("<input type='text' name='"+ data.text + "' class='form-cool'>");
+        }else if(data.password){
+            $(".gf").append("<input type='password' name='"+ data.password +"' class='form-cool'>");
+        }else if(data.email){
+            $(".gf").append("<input type='email' name='"+ data.email +"' class='form-cool'>");
+        }
+        
+    });
 });
