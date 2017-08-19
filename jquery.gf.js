9d0cd16f05cb9563f07363dc6c73dc4035a18f48
Malal <controlmalal@gmail.com>
2017-08-19 21:03:10 +0000

    suite de la fonction

--- jquery.gf.js
+++ jquery.gf.js
@@ -36,7 +36,44 @@ $(function () {
     },
     {
         "color": "couleur"
-    }]
+    },
+               {
+                   "date": "date"
+               },
+               {
+                   "time": "temps"
+               },
+               {
+                   "week": "semaine" 
+               },
+               {
+                   "month": "mois"
+               },
+               {
+                   "datetime": "datetime"
+               },
+               {
+                   "search": "recherche"
+               },
+               {
+                   "checkbox": "choix"
+               },
+               {
+                   "radio": "age"
+               },
+               {
+                   "select": "pays",
+                   "options": ["ginée", "france", "allemagne", "usa"],
+                   "selected": "allemagne"
+               },
+               {
+                   "select": "pays",
+                   "type": "1",
+                   "group": ["Afrique", "Europe", "Amerique"],
+                   "Afrique": ["Guinée", "Senegal"],
+                   "Europe": ["Allemagne", "Hollande"],
+                   "Amerique": ["USA", "Brazil"]
+               }]
     
     $.each(json, function(index, data){
         if(data.text) {
@@ -80,6 +117,56 @@ $(function () {
             }
         }else if(data.color){
              $(".gf").append("<input type='color' name='"+ data.color +"' class='form-cool'>");
+        }else if(data.date){
+             $(".gf").append("<input type='date' name='"+ data.date +"' class='form-cool'>");
+        }else if(data.time){
+            $(".gf").append("<input type='time' name='"+ data.time +"' class='form-cool'>");
+        }else if(data.week){
+            $(".gf").append("<input type='week' name='"+ data.week +"' class='form-cool'>");
+        }else if(data.month){
+            $(".gf").append("<input type='month' name='"+ data.month +"' class='form-cool'>");
+        }else if(data.datetime){
+            $(".gf").append("<input type='datetime' name='"+ data.datetime +"' class='form-cool'>");
+        }else if(data.search){
+            $(".gf").append("<input type='search' name='"+ data.search +"' class='form-cool'>");
+        }else if(data.checkbox){
+            $(".gf").append("<input type='checkbox' name='"+ data.checkbox +"' class='form-cool'>");
+        }else if(data.radio){
+            $(".gf").append("<input type='radio' name='"+ data.radio +"' class='form-cool'>");
+        }else if(data.select){
+            if(data.type){
+                if(data.type == "1"){
+                    var options;
+                    var debut = "<select name='"+ data.select +"' id='id_"+ data.select+ "'>";
+                }
+            }else{
+                var options;
+                var debut = "<select name='"+ data.select +"' id='id_"+ data.select+ "'>";
+                if(data.options){
+                    var j=1;
+                    for (var i=0; i<data.options.length; i++){
+                        if(j==1){
+                            if(data.selected && data.selected==data.options[i]){
+                                 options = "<option value='"+data.options[i]+"' selected>"+data.options[i]+"</option>";
+                            }else{
+                                 options = "<option value='"+data.options[i]+"'>"+data.options[i]+"</option>";
+                                j++;
+                            }
+                        }else{
+                            if(data.selected && data.selected==data.options[i]){
+                                options += "<option value='"+data.options[i]+"' selected>"+data.options[i]+"</option>";
+                            }else{
+                                 options += "<option value='"+data.options[i]+"'>"+data.options[i]+"</option>";
+                            }
+                        }
+                    }
+                    var app = debut + options;
+                    $(".gf").append(app);
+                }else{
+                    alert("vous devez choisir les éléments du select");
+                }
+
+            }
         }
         
     });
