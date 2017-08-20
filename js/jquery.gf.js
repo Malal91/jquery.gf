bf538dbf401decd820a033c9c1a4543dd886b579
Malal <controlmalal@gmail.com>
2017-08-20 14:07:04 +0000

    add the function of replace and ucfirst in js

--- js/jquery.gf.js
+++ js/jquery.gf.js
@@ -61,6 +61,9 @@ $(function () {
                {
                    "radio": "age"
                },
+                {
+                    "textarea": "votre_texte"
+                },
                {
                    "select": "pays",
                    "options": ["ginée", "france", "allemagne", "usa"],
@@ -77,17 +80,17 @@ $(function () {
     
     $.each(json, function(index, data){
         if(data.text) {
-            $(".gf").append("<div class='form-group'><input type='text' name='"+ data.text + "' class='form-control'>");
+            $(".gf").append("<div class='form-group'><label>"+ data.text +"</label> <input type='text' name='"+ data.text + "' class='form-control'>");
         }else if(data.password){
-            $(".gf").append("<div class='form-group'><input type='password' name='"+ data.password +"' class='form-control'>");
+            $(".gf").append("<div class='form-group'><label>"+ data.password +"</label><input type='password' name='"+ data.password +"' class='form-control'>");
         }else if(data.email){
-            $(".gf").append("<div class='form-group'><input type='email' name='"+ data.email +"' class='form-control'>");
+            $(".gf").append("<div class='form-group'><label>"+ data.email +"</label><input type='email' name='"+ data.email +"' class='form-control'>");
         }else if(data.url){
-            $(".gf").append("<div class='form-group'><input type='url' name='"+ data.url +"' class='form-control'>");
+            $(".gf").append("<div class='form-group'><label>"+ data.url +"</label><input type='url' name='"+ data.url +"' class='form-control'>");
         }else if(data.tel){
-            $(".gf").append("<div class='form-group'><input type='tel' name='"+ data.tel +"' class='form-control'>");
+            $(".gf").append("<div class='form-group'><label>"+ data.tel +"</label><input type='tel' name='"+ data.tel +"' class='form-control'>");
         }else if(data.number){
-            $(".gf").append("<div class='form-group'><input type='number' id='id_"+data.number+"' min='' max='' step='' name='"+ data.number +"' class='form-control'>");
+            $(".gf").append("<div class='form-group'><label>"+ data.number +"</label><input type='number' id='id_"+data.number+"' min='' max='' step='' name='"+ data.number +"' class='form-control'>");
             
             if(data.max){
                $("#id_"+data.number).attr('max', data.max);
@@ -102,7 +105,7 @@ $(function () {
             }
             
         }else if(data.range){
-             $(".gf").append("<div class='form-group'><input type='range' id='id_"+data.range+"' min='' max='' step='' name='"+ data.range +"' class='form-control'>");
+             $(".gf").append("<div class='form-group'><label>"+ data.range +"</label><input type='range' id='id_"+data.range+"' min='' max='' step='' name='"+ data.range +"' class='form-control'>");
             
             if(data.max){
                $("#id_"+data.range).attr('max', data.max);
@@ -116,23 +119,32 @@ $(function () {
                 $("#id_"+data.range).attr('step', data.step);
             }
         }else if(data.color){
-             $(".gf").append("<div class='form-group'><input type='color' name='"+ data.color +"' class='form-control'>");
+             $(".gf").append("<div class='form-group'><label>"+ data.color +"</label><input type='color' name='"+ data.color +"' class='form-control'>");
+        }
+        else if(data.textarea){
+            var sentence = data.textarea;
+            var result;
+            if(result = replaced(sentence)){
+                 $(".gf").append("<div class='form-group'><label>"+ result +"</label><textarea rows='8' name='"+ data.textarea +"' class='form-control'>");
+            }else{
+                $(".gf").append("<div class='form-group'><label>"+ data.textarea +"</label><textarea rows='8' name='"+ data.textarea +"' class='form-control'>");
+            }
         }else if(data.date){
-             $(".gf").append("<div class='form-group'><input type='date' name='"+ data.date +"' class='form-control'>");
+             $(".gf").append("<div class='form-group'><label>"+ data.date +"</label><input type='date' name='"+ data.date +"' class='form-control'>");
         }else if(data.time){
-            $(".gf").append("<div class='form-group'><input type='time' name='"+ data.time +"' class='form-control'>");
+            $(".gf").append("<div class='form-group'><label>"+ data.time +"</label><input type='time' name='"+ data.time +"' class='form-control'>");
         }else if(data.week){
-            $(".gf").append("<div class='form-group'><input type='week' name='"+ data.week +"' class='form-control'>");
+            $(".gf").append("<div class='form-group'><label>"+ data.week +"</label><input type='week' name='"+ data.week +"' class='form-control'>");
         }else if(data.month){
-            $(".gf").append("<div class='form-group'><input type='month' name='"+ data.month +"' class='form-control'>");
+            $(".gf").append("<div class='form-group'><label>"+ data.month +"</label><input type='month' name='"+ data.month +"' class='form-control'>");
         }else if(data.datetime){
-            $(".gf").append("<div class='form-group'><input type='datetime' name='"+ data.datetime +"' class='form-control'>");
+            $(".gf").append("<div class='form-group'><label>"+ data.datetime +"</label><input type='datetime' name='"+ data.datetime +"' class='form-control'>");
         }else if(data.search){
-            $(".gf").append("<div class='form-group'><input type='search' name='"+ data.search +"' class='form-control'>");
+            $(".gf").append("<div class='form-group'><label>"+ data.search +"</label><input type='search' name='"+ data.search +"' class='form-control'>");
         }else if(data.checkbox){
-            $(".gf").append("<div class='form-group'><input type='checkbox' name='"+ data.checkbox +"' class='form-control'>");
+            $(".gf").append("<div class='form-group'><div class='checkbox'><label><input type='checkbox' name='"+ data.checkbox +"'>"+data.checkbox);
         }else if(data.radio){
-            $(".gf").append("<div class='form-group'><input type='radio' name='"+ data.radio +"' class='form-control'>");
+            $(".gf").append("<div class='form-group'><div class='radio'><label><input type='radio' name='"+ data.radio +"'>"+data.radio);
         }else if(data.select){
             if(data.type){
                 /*if(data.type == "1"){
@@ -153,7 +165,7 @@ $(function () {
                 }*/
             }else{
                 var options;
-                var debut = "<div class='form-group'><select name='"+ data.select +"' id='id_"+ data.select+ "'>";
+                var debut = "<div class='form-group'><label>"+ data.select +"</label><select name='"+ data.select +"' id='id_"+ data.select+ "' class='form-control'>";
                 if(data.options){
                     var j=1;
                     for (var i=0; i<data.options.length; i++){
@@ -182,5 +194,17 @@ $(function () {
         }
         
     });
-    $(".gf").append("<input type='submit' class='btn btn-info' value='Envoyer'/><input type='reset' class='btn btn-info' value='Reinitialiser'/>");
+    
+    function replaced(str){
+        var result = str.replace(/_/, ' ');
+        return ucfirst(result);
+    }
+    
+    function ucfirst(str){
+        return result = str.replace(/^\w/, function($0){
+            return $0.toUpperCase();
+        });
+    }
+    
+    $(".gf").append("<input type='submit' class='btn btn-info' value='Envoyer'/> <input type='reset' class='btn btn-info' value='Reinitialiser'/>");
 });
