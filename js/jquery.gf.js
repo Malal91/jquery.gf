969c2dd99cf775302ca45209aafdd5c6317249f1
Malal <controlmalal@gmail.com>
2017-08-23 09:31:13 +0000

    modif

--- js/jquery.gf.js
+++ js/jquery.gf.js
@@ -2,41 +2,57 @@
     $.fn.gf = function(options){
         var settings = $.extend({
             connexion: null,
+            inscription: null,
+            newsletter: null,
             email: null,
+            passwordGenerate: null,
             username: null,
             action: null,
             method: null
         }, options);
         this.each(function(){
             $(this).append("<div class='gf-body'><form class='gf'></form></div>");
-            if(settings.connexion){
-                
-            }
-                        
-            if(settings.connexion) {
+            
+            if(settings.connexion && settings.connexion == 'true') {
                 if(settings.email){
                     $(".gf").append("<div class='form-group'><label for='id_"+replacedSpace(settings.email)+"'>"+ replacedUnderscore(settings.email)+"</label> <input type='email' id='id_"+replacedSpace(settings.email)+"' name='"+ replacedSpace(settings.email)+ "' class='form-gf' placeholder='"+ replacedUnderscore(settings.email)+"' required>");
                     $(".gf").append("<div class='form-group'><label for='id_password'>Mot de passe</label> <input type='password' id='id_password' name='password' class='form-gf' placeholder='Mot de passe' required>");
                     $(".gf").append("<input type='submit' class='btn btn-info' value='Envoyer'/> <input type='reset' class='btn btn-info' value='Reinitialiser'/>");
-                    if(settings.action){
-                        $(".gf").attr("action", settings.action);
-                    }
-                    if(settings.method){
-                        $(".gf").attr("method", settings.method);
-                    }
                     
                 }else if(settings.username){
                     $(".gf").append("<div class='form-group'><label for='id_"+replacedSpace(settings.username)+"'>"+ replacedUnderscore(settings.username)+"</label> <input type='text' id='id_"+replacedSpace(settings.username)+"' name='"+ replacedSpace(settings.username)+ "' class='form-gf' placeholder='"+ replacedUnderscore(settings.username)+"' required>");
                     $(".gf").append("<div class='form-group'><label for='id_password'>Mot de passe</label> <input type='password' id='id_password' name='password' class='form-gf' placeholder='Mot de passe' required>");
-                    $(".gf").append("<input type='submit' class='btn btn-info' value='Envoyer'/> <input type='reset' class='btn btn-info' value='Reinitialiser'/>");
-                    if(settings.action){
-                        $(".gf").attr("action", settings.action);
-                    }
-                    if(settings.method){
-                        $(".gf").attr("method", settings.method);
-                    }
+                    $(".gf").append("<input type='submit' class='btn btn-info' value='Se connecter'/>");
+                   
                 }
                 
+            }else if(settings.inscription && settings.inscription == 'true'){
+                if(settings.passwordGenerate && settings.passwordGenerate == 'true'){
+                     $(".gf").append("<div class='form-group'><label for='id_nom'>Nom</label> <input type='text' id='id_nom' name='nom' class='form-gf' placeholder='Nom' required>");
+                    $(".gf").append("<div class='form-group'><label for='id_prenom'>Prenom</label> <input type='text' id='id_prenom' name='prenom' class='form-gf' placeholder='Prenom' required>");
+                    $(".gf").append("<div class='form-group'><label for='id_email'>Email</label> <input type='text' id='id_email' name='email' class='form-gf' placeholder='Email' required>");
+                    
+                    $(".gf").append("<input type='submit' class='btn btn-info' value=\"S'inscrire\"/> <input type='reset' class='btn btn-info' value='Reinitialiser'/>");
+                }else{
+                     $(".gf").append("<div class='form-group'><label for='id_nom'>Nom</label> <input type='text' id='id_nom' name='nom' class='form-gf' placeholder='Nom' required>");
+                    $(".gf").append("<div class='form-group'><label for='id_prenom'>Prenom</label> <input type='text' id='id_prenom' name='prenom' class='form-gf' placeholder='Prenom' required>");
+                    $(".gf").append("<div class='form-group'><label for='id_email'>Email</label> <input type='text' id='id_email' name='email' class='form-gf' placeholder='Email' required>");
+                    $(".gf").append("<div class='form-group'><label for='id_password'>Mot de passe</label> <input type='password' id='id_password' name='password' class='form-gf' placeholder='Mot de passe' required>");
+                     $(".gf").append("<div class='form-group'><label for='id_confirm_password'>Confirmez le mot de passe</label> <input type='password' id='id_confirm_password' name='confirm_password' class='form-gf' placeholder='Confirmez le mot de passe' required>");
+                    
+                    $(".gf").append("<input type='submit' class='btn btn-info' value=\"S'inscrire\"/> <input type='reset' class='btn btn-info' value='Reinitialiser'/>");
+                }
+            }else if(settings.newsletter && settings.newsletter == 'true'){
+                $(".gf").append("<div class='form-group'><label for='id_newsletter'>Newsletter</label> <input type='email' id='id_newsletter' name='newsletter' class='form-gf' placeholder='Votre email' required>");
+                    
+                $(".gf").append("<input type='submit' class='btn btn-info' value='Souscrire'/>");
+            }
+            
+            if(settings.action){
+                $(".gf").attr("action", settings.action);
+            }
+            if(settings.method){
+                $(".gf").attr("method", settings.method);
             }
             return this;
         });
